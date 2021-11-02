416c3605eb56b408d26971a228b81f9b61f2ed95
Prakash Manglanathan <pmnathan@gmail.com>
2021-11-01 20:37:16 -0400

    Create vault-mounts.tf

---
+++ databases/db-pra-dev/vault-mounts.tf
@@ -0,0 +1,16 @@
+resource "vault_mount" "kvv2-example" {
+  path        = "version2-example"
+  type        = "kv-v2"
+  description = "This is an example KV Version 2 secret engine mount"
+}
+
+
+resource "vault_mount" "transit-example" {
+  path        = "transit-example"
+  type        = "transit"
+  description = "This is an example transit secret engine mount"
+
+  options = {
+    convergent_encryption = false
+  }
+}
